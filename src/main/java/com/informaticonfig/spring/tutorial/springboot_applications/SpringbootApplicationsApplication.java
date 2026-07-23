5dbb54781b3fa8c7abf107cee9941be8955f8279
ramonoliver <ramonolivermartos@gmail.com>
2026-07-23 09:02:07 +0200

    Initial status

---
+++ src/main/java/com/informaticonfig/spring/tutorial/springboot_applications/SpringbootApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.informaticonfig.spring.tutorial.springboot_applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplicationsApplication.class, args);
+	}
+
+}
